300b010e652da34577982b926556c9ea82bd3439
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-02-16 14:10:15 +0100

    sRGB gamma correction

--- src/process.py
+++ src/process.py
@@ -8,16 +8,32 @@ if len(sys.argv) != 2:
     print('Usage: process.py <image_file>', file=sys.stderr)
     sys.exit(1)
 
+def srgb_to_linear(image) -> np.ndarray:
+    scaled = image / 12.92
+    gamma = np.power((image + 0.055) / 1.055, 2.4)
+    return np.where(image <= 0.04045, scaled, gamma)
+
+def linear_to_srgb(image) -> np.ndarray:
+    scaled = image * 12.92
+    gamma = np.power(image, 1 / 2.4) * 1.055 - 0.055
+    return np.where(image <= 0.0031308, scaled, gamma)
+
 def save_channel(data, index, name):
     new_image = np.zeros_like(image)
     new_image[:,:,index] = data
-    cv2.imwrite(str(name), np.clip(new_image, 0, 1) * 255)
+    save_image(new_image, name)
+
+def save_image(image, name):
+    image = np.clip(image, 0, 1)
+    image = linear_to_srgb(image)
+    image *= 255
+    cv2.imwrite(str(name), image)
+
 
 source_path = Path(sys.argv[1])
 image = cv2.imread(str(source_path), cv2.IMREAD_COLOR) / 255.0
 
-gamma = 1.5
-image = image ** gamma
+image = srgb_to_linear(image)
 
 target_path_base = Path(source_path.name)
 
@@ -36,12 +52,15 @@ for index, name in enumerate('BGR'):
         if sub_index == index:
             continue
         sub_channel = image[:,:,sub_index]
-        channel -= sub_channel * 0.5
+        channel -= sub_channel
     new_path = target_path_base.with_stem(target_path_base.stem + '_' + name + '_sub')
     save_channel(channel, index, new_path)
+    if name == 'G':
+        green_channel = channel
 
     combined_image[:,:,index] = channel
 
-cv2.imwrite(str(target_path_base.with_stem(target_path_base.stem + '_combined')), np.clip(combined_image, 0, 1) * 255)
+save_image(combined_image, target_path_base.with_stem(target_path_base.stem + '_combined'))
+
 
 print('Done.')
