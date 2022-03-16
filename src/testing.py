879fe4af22eb7d7a8f27ca113f49bde39b6b35bf
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-03-16 12:41:33 +0100

    image est testing

--- src/testing.py
+++ src/testing.py
@@ -1,19 +1,32 @@
 import numpy as np
-from process import save_image, load_image
+from process import ValidationResult, save_image, load_image
+from parameter_estimation import do_parameter_est
 
 def main():
     image = load_image('output.bak/2020-01-10-10-36-57_image_correct0.png')
+    diffed = load_image('output.bak/2020-01-10-10-36-57_image_correct0_diff.png')
     
     image_B = image[:, :, 0]
     image_G = image[:, :, 1]
     image_R = image[:, :, 2]
-    print(image_R[16, 16])
 
-    brightest = np.unravel_index(np.argmax(image_B), image_B.shape)
-    print(brightest)
-    x, y = brightest
-    image_R[x, y] = 1
+    brighest_green = (image.shape[0] // 2, image.shape[1] // 2)
+    brightest_blue = np.unravel_index(np.argmax(image_B), image_B.shape)
+    brightest_red = np.unravel_index(np.argmax(image_R), image_R.shape)
+
+    image_B[brightest_red[0], brightest_red[1]] = 1
+    image_R[brightest_blue[0], brightest_blue[1]] = 1
     save_image(image, 'tmp.png')
+
+    result = ValidationResult(
+        valid=True,
+        green_center=brighest_green,
+        blue_center=brightest_blue,
+        red_center=brightest_red,
+        radius=3
+    )
+
+    do_parameter_est(image, diffed, result, "parameters.txt")
 
 
 if __name__ == '__main__':
